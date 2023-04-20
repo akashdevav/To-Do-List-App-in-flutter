7e493485cc39cefd10f7b96fe9da43a4229276c5
akashdevav <akashofficialone@gmail.com>
2023-04-20 23:24:39 +0530

    added calender icon

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,13 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Home());
 
-class Home extends StatelessWidget
-{
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+        centerTitle: true,
+        title: const Text('To-Do List'),
+        actions: [
+          IconButton(
+              onPressed: null,
+               icon: Icon(CupertinoIcons.calendar_circle_fill))
+        ],
+      ),
+      body: Column(mainAxisSize: MainAxisSize.min,),
+      ),
+    );
   }
 }

b8c635c27a21b6c9a1b9458ae4a8aa20e2c2fb39
akashdevav <akashofficialone@gmail.com>
2023-04-20 14:38:37 +0530

    started todo app in flutter

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Home());
+
+class Home extends StatelessWidget
+{
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
